4fd5b3499174afdbb0b1424db3250658dc69a864
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-07 17:22:19 -0500

    Build function to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string = '') {
+    const wordArray = string.split('');
+    return wordArray.reverse().join('');
 };
 
 // Do not edit below this line
