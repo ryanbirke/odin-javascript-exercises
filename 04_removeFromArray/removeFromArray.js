d59a542b85ed7cfc89cc728ad0a480eb53e3547c
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-07 18:01:54 -0500

    Create function to remove items from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray = [], ...searchItems) {
+    console.log(searchItems)
+    for (const searchItem of searchItems) {
+        let index = -1;
+        while ((index = inputArray.indexOf(searchItem)) > -1) {
+            inputArray.splice(index, 1);
+        } 
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
