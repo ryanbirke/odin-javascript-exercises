73270ad2a2072814a95449f1a3fadce40497f299
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-07 16:43:26 -0500

    Fix test errors for 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string = '', num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        array = []
+        for (let i = 0; i < num; i++) {
+            array[i] = string
+        }
+        return array.join('');
+    }
 };
 
 // Do not edit below this line
