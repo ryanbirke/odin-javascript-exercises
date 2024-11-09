664316b5132f94d91f1b3a7b277c5bee4951d65f
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-09 12:43:14 -0500

    Create function to check leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const a = ((year % 400) === 0);
+    const b = (((year % 4) === 0) && (!((year % 100) === 0)));
+    return (a || b);
 };
 
 // Do not edit below this line
