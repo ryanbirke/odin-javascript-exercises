3383630fc37caba2574c4bd7833e40c72fcbb5fc
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-09 12:20:57 -0500

    Create function to sum between two ints

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (!((Number.isInteger(int1) && (int1 > 0)) && 
+        (Number.isInteger(int2) && (int2 > 0)))) {
+        return 'ERROR';
+    } else if (int1 === int2) {
+        return int1;
+    } else {
+        const intMax = Math.max(int1, int2);
+        const intMin = Math.min(int1, int2);
 
+        array = [];
+
+        for (let i = intMin; i <= intMax; i++) {
+            array.push(i)
+        }
+        
+        return array.reduce((sum, num) => sum + num)
+    }
 };
 
 // Do not edit below this line
