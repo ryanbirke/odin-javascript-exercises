c536cd678d3c3410b45363b92e0793156fca7cfe
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-09 12:48:29 -0500

    Create functions to convert between f to c

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(startingTemp) {
+  let endTemp = ((startingTemp - 32) * 5 / 9);
+  return +(endTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(startingTemp) {
+  let endTemp = ((startingTemp * 9 / 5) + 32);
+  return +(endTemp.toFixed(1));
 };
 
 // Do not edit below this line
